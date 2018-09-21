398d3e509c5cc1e52b827209c29133f9d992246b
pasitthanadamkerng <5910110216@psu.ac.th>
2018-09-21 15:45:22 +0700

    ปรับปรุง - เพิ่ม ChinjungGame

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
+import ChinjungGame from './ChinjungGame';
 
-const word = "Hello";
+const word = "Pasitthandamkerng";
+const chin = "5910110216"
 class App extends Component{
   render(){
     return(
       <div>
         <WordCard value={word}/>
+        <ChinjungGame value={chin}/>
       </div>
     );
   }
