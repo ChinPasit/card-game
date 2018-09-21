15eb08ac8988489e2f44912fd3d9134bf85ed74b
pasitthanadamkerng <5910110216@psu.ac.th>
2018-09-21 15:43:34 +0700

    ปรับปรุง - เพิ่ม CharacterChinjung

---
+++ src/CharacterChinjung.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class CharacterChinjung extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            active : false,
+        }
+    }
+
+    activate = () => {
+        if(!this.state.active){
+            this.props.activationHandler(this.props.value)
+            this.setState({active: true})
+        }
+    }
+    componentDidUpdate(prevProps){
+        if(prevProps.attemt !== this.props.attemt){
+          this.setState({active:false})
+        }
+      }
+    render(){
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return(
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
+    }
+}
